29034cb955e49594b47122da858dfc759668c602
radhey113 <radheyg11@gmail.com>
2018-09-30 02:16:47 +0530

    package-lock json

--- index.js
+++ index.js
@@ -6,35 +6,39 @@ var pdf = require('html-pdf');
 const base64 = require('base64-stream');
 
 let app = express();
+let port = process.env.port || 3002;
 app.use('/', express.static(path.join(__dirname, './')));
 
 /** upload project...  **/ 
 
-app.get('/checkpdf', (req, res) => {
-    console.log('sdfasfafa: ', req.params);
+app.get('/pdf', (req, res) => {
+
     let htmlRead = fs.readFile('./index.html', 'utf8', (err, data) => {
         conversion({ html: data }, (err, pdf) => {
-            let name = Date.now() + 'google.pdf';
+            let name = Date.now() + '.pdf';
             let file = fs.createWriteStream(name, {flag: "wx"});
             let base;    
-            // base = pdf.stream.pipe(file)
             // base = pdf.stream.pipe(base64.encode()).pipe(file)
-             base = pdf.stream.pipe(file)
+            pdf.stream.pipe(file)
                 .on('finish', () => {
                     let base64data = fs.readFileSync(name, 'utf8');        
                     base64data = 'data:application/pdf;base64,' + base64data;
                     // res.status(301).jsonp({base: base64data})
-                    let pathpdf = 'http://localhost:3002/' + name;
-                    res.redirect(pathpdf);
+                    let pathpdf = 'http://192.168.43.139:3002/' + name;
+                    
                     setTimeout(() => {
                       fs.unlinkSync('./' + name);
                     }, 10000);
+                    res.jsonp({
+                        url: pathpdf,
+                        name: name
+                    });
                 });
         })
     })
 });
 
 /** check process env port **/ 
-app.listen(process.env.PORT || 3002, ()=> {
-    console.log(`Project is running... ${process.env.PORT || 3001}`);
+app.listen(port, ()=> {
+    console.log(`Project is running... ${port}`);
 });
